5fe82eeb53f4461d14e63e18aaea5c5c802d7c78
ShakhzodbekYuldoshov <shahzodyoldoshov10@gmail.com>
2022-02-09 20:53:12 +0500

    normalizations compared and errors resolved

--- NormalizeAndSave.py
+++ NormalizeAndSave.py
@@ -1,6 +1,7 @@
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
+
 def normalize_and_save(df, drop_col_names, save_path):
     dropped_col_values = []
 
@@ -12,20 +13,21 @@ def normalize_and_save(df, drop_col_names, save_path):
             df.drop(col_name, inplace=True, axis=1)
         except:
             print('Column not found:  ', col_name)
-    
+
     # normalize dataframe between 0 and 1
     scaler = MinMaxScaler()
     scaled_df = scaler.fit_transform(df)
 
     # prepared dropped columns and rescaled columns to be able to
     # concatenate and create new dataframe
-    dropped_df = pd.DataFrame.from_dict(dict(zip(drop_col_names, dropped_col_values)))
+    dropped_df = pd.DataFrame.from_dict(
+        dict(zip(drop_col_names, dropped_col_values)))
     scaled_df = pd.DataFrame.from_dict(dict(zip(df.columns, scaled_df.T)))
 
-    # concatenate dropped and scaled 
+    # concatenate dropped and scaled
     concatenated_df = pd.concat([dropped_df, scaled_df], axis=1)
 
-    # save as csv 
-    concatenated_df.to_csv(save_path)
+    # save as csv
+    concatenated_df.to_csv(save_path, index=False)
 
     return concatenated_df
--- Preprocessing.py
+++ Preprocessing.py
@@ -2,6 +2,9 @@ from NormalizeAndSave import normalize_and_save
 import pandas as pd
 
 
+# load dataset
 df = pd.read_csv('./dataset/bodyPerformanceI.csv')
 
-normalize_and_save(df, ['gender', 'class'], './dataset/bodyPerformanceNormalized.csv')
+# normalize dataset to be able to reach minimum while training
+normalized_df = normalize_and_save(
+    df, ['gender', 'class'], './dataset/bodyPerformanceNormalized.csv')
